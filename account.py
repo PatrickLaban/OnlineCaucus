95d36af72baa2e9266b10f99bc52d9a0b2cc8b8d
Patrick Laban <laban.patrck@gmail.com>
2016-04-16 18:14:01 -0700

    Now able to create a simple caucus model

--- account.py
+++ account.py
@@ -3,9 +3,13 @@ import webapp2
 import controllers.account.account_index
 import controllers.account.company
 import controllers.account.location
+import controllers.account.caucus
 
 app = webapp2.WSGIApplication(
-    [('/account/company_create/?', controllers.account.company.CompanyCreateHandler),
+    [('/account/caucus_create/?', controllers.account.caucus.CaucusCreateHandler),
+     ('/account/caucus_edit/(\d+)', controllers.account.caucus.CaucusEditHandler),
+     ('/account/caucus/(\d+)', controllers.account.caucus.CaucusInfoHandler),
+     ('/account/company_create/?', controllers.account.company.CompanyCreateHandler),
      ('/account/company_edit/(\d+)', controllers.account.company.CompanyEditHandler),
      ('/account/company/(\d+)', controllers.account.company.CompanyInfoHandler),
      ('/account/location_create/?', controllers.account.location.LocationCreateHandler),
--- models/Caucus.py
+++ models/Caucus.py
@@ -12,10 +12,10 @@ class Caucus(ModelBase):
 
     @classmethod
     def create_caucus(cls, name, user_account):
-        new_caucus = cls(name=name, created_by=user_account)
+        new_caucus = cls(name=name, created_by=user_account.key)
         new_caucus.put()
         return new_caucus
 
     def add_participant(self, user_account):
-        self.participants.append = user_account
+        self.participants.append = user_account.key
         self.put()
